5d750c661bfb1c9c4ad2fed99b112bebe5d7751b
Mohaned <memo.yossry@gmail.com>
2023-04-01 14:16:54 +0200

    feat: Add FlutterDisplayMode config

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:example/app/index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 
-void main() {
+void main()async {
   runApp(const MyApp());
+  await FlutterDisplayMode.setHighRefreshRate();
 }
