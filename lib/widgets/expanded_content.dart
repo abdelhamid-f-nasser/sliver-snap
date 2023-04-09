80d48419d3d0a9f6a118cdd3694ef810a80716ae
A-Fawzyy <fawzy3171@gmail.com>
2023-04-09 09:57:53 +0200

    📝 docs(SliverSnap): Improve docs for Expanded and Collapsed widgets

--- lib/widgets/expanded_content.dart
+++ lib/widgets/expanded_content.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_snap/sliver_snap.dart';
 
 /// Widget which is shown when the [SliverSnap] is expanded.
 ///
 /// Helps in adding a leading widget only in the expanded state.
 class ExpandedContent extends StatelessWidget {
+  /// The child widget of the [ExpandedContent].
+  ///
   final Widget? child;
+
+  /// The leading widget of the [ExpandedContent].
+  ///
+  /// It is usually a [BackButton] or a [CloseButton].
+  ///
+  /// Use this only when you need a leading widget only in the expanded state.
+  ///
+  /// Take care when adding the [leading] widget both here and
+  /// in the [SliverSnap] widget, as they might overlap.
   final Widget? leading;
 
   const ExpandedContent({
